212c0b17866067a9e98a8385356bb3c509ec8289
cyntia <cyntiajasmine7@gmail.com>
2024-04-26 16:47:47 +0700

    Initial Commit

---
+++ cadoptio/View/Presenter/PetAdoptionView.swift
@@ -0,0 +1,70 @@
+//
+//  PetAdoptionView.swift
+//  cadoptio
+//
+//  Created by David Gunawan on 22/03/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct PetAdoptionView: View {
+    
+    @State private var isFilterViewPresented = false
+    @State private var isAddNewPetViewPresented = false
+    
+    @Query var pets: [PetModel]
+    
+    var body: some View {
+        NavigationStack {
+            List {
+//                PetCardView(pet: PetModel(
+//                    name: "Leppy",
+//                    breed: "Domestic",
+//                    weight: "3 kg",
+//                    gender: "Female",
+//                    imageName: "Leppy")
+//                )
+//                PetCardView(pet: PetModel(
+//                    name: "Sky",
+//                    breed: "Ragdoll",
+//                    weight: "5 kg",
+//                    gender: "Male",
+//                    imageName: "Sky")
+//                )
+                //Text("number of pet \(pets.count)")
+            }
+            .listStyle(.plain)
+            .navigationTitle("My Pet")
+            .toolbar {
+                Button(action: {
+                    isAddNewPetViewPresented = true
+                }) {
+                    Image(systemName: "plus")
+                        .foregroundColor(.orange)
+                }
+                Button(action: {
+                    isFilterViewPresented = true
+                }) {
+                    Image(systemName: "line.3.horizontal.decrease.circle.fill")
+                        .foregroundColor(.orange)
+                }
+            }
+            .sheet(isPresented: $isFilterViewPresented) {
+                FilterView()
+            }
+            .sheet(isPresented: $isAddNewPetViewPresented) {
+                AddNewPetView()
+            }
+        }
+    }
+}
+
+struct PetAdoptionView_Previews: PreviewProvider {
+    static var previews: some View {
+//        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+//        let container = try! ModelContainer(for: PetModel.self, configurations: config)
+        
+        PetAdoptionView()
+    }
+}
---
+++ cadoptio/cadoptioApp.swift
@@ -0,0 +1,20 @@
+//
+//  cadoptioApp.swift
+//  cadoptio
+//
+//  Created by David Gunawan on 22/03/24.
+//
+
+import SwiftUI
+import SwiftData
+
+
+
+@main
+struct cadoptioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PetAdoptionView()
+        }
+    }
+}
